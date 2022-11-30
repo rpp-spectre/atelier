64ef6bde735d9d9c055a29e994bd62693c6e1515
hutong <hutong@gmail.com>
2022-11-30 14:06:36 -0600

    clean up in enviroment cleanup

--- client/dist/bundle.js
+++ client/dist/bundle.js
@@ -7,8 +7,26 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
-/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./client/src/index.jsx":
+/*!******************************!*\
+  !*** ./client/src/index.jsx ***!
+  \******************************/
+/***/ (() => {
+
+eval("\n\n//# sourceURL=webpack://atelier/./client/src/index.jsx?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
 /******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./client/src/index.jsx"]();
 /******/ 	
 /******/ })()
 ;
