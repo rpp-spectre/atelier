7b3796ac277cd8a5908dc1dd5efa6b1094e2ce5f
Jacinthe Chong <raghavjacinthe@gmail.com>
2022-12-06 23:10:02 -0500

    adding local dotenv file and uncommented server.js code

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-// const controllerQ = require('./controllers/questions');
+const controllerQ = require('./controllers/questions');
 
 
 const app = express();
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.static("./client/dist"));
 app.use(express.json());
 
-// app.get("/questions", controllerQ.getQuestions);
-// app.get("/questions/640996/answers", controllerQ.getAnswers);
+app.get("/questions", controllerQ.getQuestions);
+app.get("/questions/640996/answers", controllerQ.getAnswers);
 
 app.listen(3000);
