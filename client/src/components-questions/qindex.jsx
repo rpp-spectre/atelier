6463ae56a9a11a528982ca9e12fdf2ce0812760b
hutong <hutong@gmail.com>
2022-12-06 23:04:58 -0600

    move load more questions button

--- client/src/components-questions/qindex.jsx
+++ client/src/components-questions/qindex.jsx
@@ -1,18 +1,50 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
 import ReacDom from 'react-dom/client';
 import Qlist from './qlist.jsx';
 import Qsearch from './qsearch.jsx';
 import Qadd from './addforms/qadd.jsx';
 import Alist from './alist.jsx';
+import axios from 'axios';
+import 'regenerator-runtime/runtime.js';
 
 const Qsection = ()=>{
+  const[noQ,setNoQ]=useState(2);
+  const[questions,setQuestions]=useState([]);
+
+  let load = ()=>{
+    if (noQ >= questions.length) {
+      return null;
+    } else {
+      return <button onClick={()=>{setNoQ(noQ+2)}}>LOAD MORE QUESTIONS</button>;
+    }
+  };
+
+  useEffect(()=>{
+    (async()=>{
+      let response = await axios.get("http://localhost:3000/QUESTIONS");
+      // console.log('response', response.data.results[0]);
+      let qs=response.data.results;
+      qs.sort(function(a, b) {
+        if(a['question_helpfulness']> b['question_helpfulness']){
+          return -1;
+        }
+        if(a['question_helpfulness']< b['question_helpfulness']){
+          return 1;
+        }
+        return 0;
+      });
+      setQuestions(qs);
+      // setQuestions(response.data.results);
+    })()
+  },[]);
+
   return (<div  data-testid="Qindex" className="question">
     <h2>QUESTIONS & ANSWERS</h2>
     <Qsearch />
     <div className='question-list'>
-    <Qlist />
+    <Qlist noQ={noQ} questions={questions}/>
     </div>
-
+    {load()}
 
     <Qadd />
   </div>);
--- client/src/components-questions/qlist.jsx
+++ client/src/components-questions/qlist.jsx
@@ -1,52 +1,20 @@
 import React,{useState, useEffect} from 'react';
 import ReactDom from 'react-dom/client';
-import axios from 'axios';
-import 'regenerator-runtime/runtime.js';
 // require("dotenv").config();
 import Q from './q.jsx';
 
 
 
-var Qlist = () =>{
-  const[questions,setQuestions]=useState([]);
-  const[NoQ,setNoQ]=useState(2);
-
-  let load = ()=>{
-    if (NoQ >= questions.length) {
-      return null;
-    } else {
-      return <button onClick={()=>{setNoQ(NoQ+2)}}>LOAD MORE QUESTIONS</button>;
-    }
-  };
-
-
-  useEffect(()=>{
-    (async()=>{
-      let response = await axios.get("http://localhost:3000/QUESTIONS");
-      // console.log('response', response.data.results[0]);
-      let qs=response.data.results;
-      qs.sort(function(a, b) {
-        if(a['question_helpfulness']> b['question_helpfulness']){
-          return -1;
-        }
-        if(a['question_helpfulness']< b['question_helpfulness']){
-          return 1;
-        }
-        return 0;
-      });
-      setQuestions(qs);
-      // setQuestions(response.data.results);
-    })()
-  },[]);
+var Qlist = ({noQ, questions}) =>{
 
   return (<div>
 
-       {questions.slice(0,NoQ).map((question)=>{
+       {questions.slice(0,noQ).map((question)=>{
 
         return <Q question={question} key = {question.question_id} />
       })}
 
-     {load()}
+
 
   </div>);
 };
