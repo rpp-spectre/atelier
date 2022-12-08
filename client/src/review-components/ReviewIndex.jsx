1feb2c77625cc088d0cf98026fcb155e7f28b24b
Jacinthe Chong <raghavjacinthe@gmail.com>
2022-12-08 00:39:19 -0500

    Reviews Component served by API

--- client/src/review-components/ReviewIndex.jsx
+++ client/src/review-components/ReviewIndex.jsx
@@ -1,5 +1,6 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom/client';
+import axios from 'axios';
 import ReviewList from './ReviewList.jsx';
 import ReviewForm from './ReviewForm.jsx';
 import Review from './Reviews.jsx';
@@ -7,11 +8,12 @@ import Ratings from './Ratings.jsx';
 import ProductBreakdown from './ProductBreakdown.jsx';
 
 function ReviewSection(props) {
-  let totalReviewsArray = [<Review />, <Review />, <Review />, <Review />, <Review />];
-  const [reviewCount, setReviewCount] = useState(totalReviewsArray.length);
-  const [addReviews, setAddReviews]  = useState(false);
+  const [totalReviewsArray, setReviewsArray] = useState([]);
   const [showReviewCount, sliceReviewArray] = useState(2);
   let shownReviewsArray = totalReviewsArray.slice(0, showReviewCount);
+  const [reviewCount, setReviewCount] = useState(totalReviewsArray.length);
+  const [addReviews, setAddReviews]  = useState(false);
+  const [reviewMeta, setReviewMeta] = useState(null);
 
   let addReviewButton = <button onClick={() => {setAddReviews(true)}}>Add A Review +</button>;
   if (addReviews === true) {
@@ -23,11 +25,41 @@ function ReviewSection(props) {
     moreReviewButton = null;
   }
 
+  let reviewTiles = <ReviewList props={shownReviewsArray}/>
+  if (totalReviewsArray.length === 0) {
+    reviewTiles = null;
+  }
+
+  let ratingSection = <Ratings data={reviewMeta}/>
+  if (!reviewMeta) {
+    ratingSection = null;
+  }
+
+  useEffect(() => {
+    (async() => {
+      let result = await axios.get('http://localhost:3000/reviewsMeta');
+      setReviewMeta(result.data);
+    })()
+  }, []);
+
+  useEffect(() => {
+    (async() => {
+      let result = await axios.get('http://localhost:3000/reviews');
+      setReviewCount(result.data.count);
+      let resultArray = [];
+      result.data.results.forEach((element) => {
+        console.log(element);
+        resultArray.push(<Review data={element}/>);
+      });
+      setReviewsArray(resultArray);
+    })()
+  }, []);
+
   return (
   <div>
     <h2>Reviews and Ratings</h2>
     <div className='rating-product-breakdown'>
-    <Ratings />
+    {ratingSection}
     <ProductBreakdown />
     <ProductBreakdown />
     <ProductBreakdown />
@@ -41,7 +73,7 @@ function ReviewSection(props) {
         </select>
       </h3>
         <div className='review-list'>
-          <ReviewList props={shownReviewsArray}/>
+          {reviewTiles}
         </div>
         <div className='review-buttons'>
           {moreReviewButton}
--- server/index.js
+++ server/index.js
@@ -1,13 +1,21 @@
-const express = require("express");
+const express = require('express');
 const controllerQ = require('./controllers/questions');
+const reviews = require('./controllers/review.js');
 
 
 const app = express();
 
-app.use(express.static("./client/dist"));
+app.use(express.static('./client/dist'));
 app.use(express.json());
 
-app.get("/questions", controllerQ.getQuestions);
-app.get("/questions/:qid/answers", controllerQ.getAnswers);
+//Product Overview
+
+//Reviews
+app.get('/reviews', reviews.getReviews);
+app.get('/reviewsMeta', reviews.getReviewMeta);
+
+//Questions and Answers
+app.get('/questions', controllerQ.getQuestions);
+app.get('/questions/:qid/answers', controllerQ.getAnswers);
 
 app.listen(3000);
